f77266e8b6554f22bfc4647e2f5ee01264252587
федор соснин <sosninfedor@gmail.com>
2024-11-05 23:25:30 +0500

    Task1

---
+++ Tyuiu.SosninFM.Sprint4.Task1.V8/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Tyuiu.SosninFM.Sprint4.Task1.V8.Lib;
+
+namespace Tyuiu.SosninFM.Sprint4.Task1.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
